8fb0cd0799d184b9603ce8335ebf3e93b1d66653
telebob <info71@list.ru>
2020-08-03 13:16:07 +0300

    qqq

---
+++ frontend/src/app/food-product/food-product.module.ts
@@ -0,0 +1,10 @@
+import { FoodProductsModule } from './../food-products/food-products.module';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: []
+})
+
+export class FoodProductModule {}
